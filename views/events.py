d9dec4eff9812afcab947fb0b3ac344f48bee26d
Zian Wang <wza15046319911@github.com>
2022-10-10 15:24:08 +1100

    Ignore .idea files

--- views/events.py
+++ views/events.py
@@ -5,8 +5,7 @@ from pymongo.collection import Collection
 # from rest_framework.response import Response
 # from rest_framework.decorators import api_view, renderer_classes
 # from rest_framework.renderers import JSONRenderer
-from rest_framework.decorators import api_view
-
+# from rest_framework.decorators import api_view
 from . import config
 from .encoders import MongoJsonEncoder, insert
 import json
